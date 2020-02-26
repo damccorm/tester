b0ccd22cf1a47cdb72d680eb5eb9be3468c0862a
damccorm <damccorm@microsoft.com>
2020-02-26 17:10:18 -0500

    Convert to node

---
+++ .github/labelChecker/index.js
@@ -0,0 +1,31 @@
+const rm = require('typed-rest-client/RestClient');
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function main() {
+    try {
+        const pullRequestNumber = github.context.issue.number;
+        let rest = new rm.RestClient('labelChecker');
+        let res = await rest.get(`https://api.github.com/repos/microsoft/azure-pipelines-agent/issues/${pullRequestNumber}/labels`);
+        let labelCount = 0;
+        res.result.forEach(tag => {
+            let name = tag.name.toLowerCase();
+            if (['bug', 'enhancement', 'misc', 'internal'].indexOf(name) > -1) {
+                console.log(`Found tag: ${name}`);
+                labelCount++;
+            }
+        });
+
+        if (labelCount === 0) {
+            throw 'Must be labeled one of [bug, enhancement, misc, internal]'
+        }
+        if (labelCount > 1) {
+            throw `Cannot contain more than one label of [bug, enhancement, misc, internal]. Currently contains ${labelCount}`
+        }
+    } catch (err) {
+        core.setFailed(err);
+    }
+
+}
+
+main();
